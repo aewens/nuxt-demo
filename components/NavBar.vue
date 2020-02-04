61eb6ee5af00cc946719255c7a2c2c5dd47acf0f
aewens <email@aewens.com>
2020-02-04 16:32:27 -0600

    Added nav-bar component, cleared out default page content

---
+++ components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="flex items-center justify-between flex-wrap bg-black p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Demo</span>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <nuxt-link to="/dashboard" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4">
+            Dashboard
+          </nuxt-link>
+          <nuxt-link to="/blog" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4">
+            Blog
+          </nuxt-link>
+          <nuxt-link to="/about" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4">
+            About
+          </nuxt-link>
+        </div>
+        <div>
+          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0">Download</a>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "nav-bar"//,
+    // data () {
+    //     return {};
+    // }
+};
+</script>
+
+<style>
+</style>
